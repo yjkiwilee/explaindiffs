f27a34404622241c81d82b0f9589ac89bcc20eb1
Young Jun Lee <yjkiwilee@gmail.com>
2024-08-08 14:06:17 +0100

    Update test script

--- confmat_inat.py
+++ confmat_inat.py
@@ -1,51 +1,9 @@
-from typing import List
+from common_scripts import inat_api
 import json
-import requests as rq
-import time
 
-def get_id_histories(taxon:str, n_obs:int, obs_per_page:int = 200) -> List[List[str]]:
-    """
-    Retrieve the identification histories of the observations of a given taxon.
+# id_histories = inat_api.get_id_histories('Syntrichia laevipila', 1000)
 
-    Parameters:
-        taxon (str): The acientfic of the taxon of interest.
-        n_obs (int): The number of observations to look up using the iNat API.
-        obs_per_page (int): The number of observations per page to use for the API call. Deafult is 200.
+# with open('data/test_data.json', 'w') as fp:
+#     fp.write(json.dumps(id_histories))
 
-    Returns:
-
-    """
-
-    # Get identification histories for a taxon
-    id_histories = []
-    for page_id in range(0, n_obs // obs_per_page + 1): # Iterate through page ids
-        # Make request to retrieve observations of the taxon
-        obs = rq.get('https://api.inaturalist.org/v1/observations',
-                        params = {
-                            'taxon_name': taxon,
-                            'per_page': min(obs_per_page, n_obs - page_id * obs_per_page),
-                            'page': page_id + 1,
-                            'identified': 'true', # Observations with community identifications
-                            'quality_grade': 'research' # Filter for research-grade observations, so that we are reasonably confident of the 'true' identity of the species
-                        }).json()['results']
-
-        # Convert to a list of identification histories
-        id_histories_part = [
-            [ident['taxon']['name'] for ident in obs_one['identifications']]
-            for obs_one in obs
-        ]
-
-        # Push to id_histories
-        id_histories.extend(id_histories_part)
-
-        # Log
-        print('Done batch {}'.format(page_id))
-
-        time.sleep(1) # Sleep to fit iNaturalist's API Terms of Use ()
-    
-    return id_histories
-
-id_histories = get_id_histories('Rumex', 1000)
-
-with open('data/test_data.json', 'w') as fp:
-    fp.write(json.dumps(id_histories))
+print(inat_api.get_sim_spp(55821))
